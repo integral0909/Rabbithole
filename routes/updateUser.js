6d5bd3a6078c3d072260b59df08b8fdfc967b8ba
whitesnow0505 <whitesnow0505@zoho.com>
2017-11-24 23:14:35 +0800

    ---

--- routes/updateUser.js
+++ routes/updateUser.js
@@ -12,18 +12,15 @@ const router = express.Router();
  * Update user FBFriends
  */
 
-router.post('/update_fbFriends', (req, res, next) => {     
+router.post('/update_username', (req, res, next) => {     
     const userId = req.body.user_id;
-    const friends = req.body.friends;
     const username = req.body.username;
-    const isFlagged = req.body.isFlagged;
 
     Account.findByIdAndUpdate(userId,
         {
-            $set: {username: username, isFlagged: isFlagged, isVerified: true},
-            $push: { fbFriends: { $each: friends}}
-        },
-        {safe: true, upsert: true, new: true, multi: true }).then (account =>{
+            $set: {username: username, isVerified: true},
+        })
+        .then (account =>{
             if (account) {
                 res.json(ResponseResult.getResoponseResult(ResponseResult.customizedUserInfo(account), 1, "success"));
             }else {
